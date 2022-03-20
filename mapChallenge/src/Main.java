074e2fcfa1dfd0736e214c59ecaea62397c84459
talisbreda <talis.breda@outlook.com>
2022-03-20 17:52:23 -0300

    Finished the challenge related to Maps

--- mapChallenge/src/Main.java
+++ mapChallenge/src/Main.java
@@ -8,18 +8,20 @@ public class Main {
 
     public static void main(String[] args) {
         Random rand = new Random();
-        Map<Integer, Integer> dados = new HashMap<>();
-        Map<Integer, Integer> count = new LinkedHashMap<>();
+        Map<Integer, Integer> dados = new TreeMap<>();
+        Map<Integer, Integer> count = new TreeMap<>();
         for (int i = 0; i < 100; i++) {
-            dados.put(rand.nextInt(100), rand.nextInt(10));
+            dados.put(i, rand.nextInt(10));
         }
         for (Map.Entry<Integer, Integer> dado : dados.entrySet()) {
             if (count.containsKey(dado.getValue())) {
                 count.put(dado.getValue(), count.get(dado.getValue()) + 1);
             } else {
-                count.put(dado.getValue(), 0);
+                count.put(dado.getValue(), 1);
             }
         }
-        System.out.println(count);
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            System.out.printf("%d: %d vezes%n", entry.getKey(), entry.getValue());
+        }
     }
 }
